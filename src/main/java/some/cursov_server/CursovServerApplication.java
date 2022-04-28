1c0fdc1d29bee6952819ab033dca0f6a89fbc5d2
vadniks <vadniks@fk.fk>
2022-04-29 00:30:27 +0300

    Programming the MVC architecture

--- src/main/java/some/cursov_server/CursovServerApplication.java
+++ src/main/java/some/cursov_server/CursovServerApplication.java
@@ -3,18 +3,16 @@ package some.cursov_server;
 import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.Properties;
 
-@EntityScan("some.cursov_server.*")
 @SpringBootApplication
 public class CursovServerApplication {
 
     public static void main(String... args) {
         val app = new SpringApplication(CursovServerApplication.class);
         val props = new Properties();
-        props.setProperty("debug", "true");
+//        props.setProperty("debug", "true");
         app.setDefaultProperties(props);
         app.run(args);
     }
