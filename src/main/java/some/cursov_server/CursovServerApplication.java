f96d6523d2fd04e04880baae50ed8f756573c812
vadniks <vadniks@fk.fk>
2022-04-28 15:32:55 +0300

    Initial commit

---
+++ src/main/java/some/cursov_server/CursovServerApplication.java
@@ -0,0 +1,12 @@
+package some.cursov_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursovServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursovServerApplication.class, args);
+    }
+}
---
+++ src/main/java/some/cursov_server/ui/MainView.java
@@ -0,0 +1,8 @@
+package some.cursov_server.ui;
+
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView {
+
+}
