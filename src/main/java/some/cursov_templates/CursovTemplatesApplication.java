e4c92f02f4ba0b00729f082babf5b5083ad5d2c6
vadniks <vadniks@fk.fk>
2022-05-04 13:16:44 +0300

    Started alternative version of app

---
+++ src/main/java/some/cursov_templates/CursovTemplatesApplication.java
@@ -0,0 +1,13 @@
+package some.cursov_templates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursovTemplatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursovTemplatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/some/cursov_templates/CursovTemplatesApplicationTests.java
@@ -0,0 +1,13 @@
+package some.cursov_templates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursovTemplatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
