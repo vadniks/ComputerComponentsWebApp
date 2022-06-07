5feb581836c6773fa7bc15ea3d5322762f98dcae
vadniks <vadniks@fk.fk>
2022-06-07 22:02:42 +0300

    Adding cart

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -8,3 +8,24 @@ function rqs(whr) { G.request(
 
 window.logOut = function logOut() { rqs(G.lgo) }
 window.onClear = function onClear() { rqs(G.clr) }
+
+function elm(a) { return document.getElementById(a) }
+
+const sbt = elm('sbt'), fnm = elm('fnm'), lnm = elm('lnm'),
+    phn = elm('phn'), adr = elm('adr'), ttl = elm('sbtTtl'),
+    ord = elm('sbtOrd'), ovr = elm('overlay')
+
+function opn(a) {
+    sbt.style.display = a ? 'flex' : 'none'
+    ovr.style.display = a ? 'flex' : 'none'
+}
+
+function pld() {
+
+}
+
+window.order = () => { G.request(G.ps, '/ord', () => {
+
+}, pld(), () => {
+
+}) }
