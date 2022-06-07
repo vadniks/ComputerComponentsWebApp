aa6e12c4f1be641697f12bf0f9852772910b7b9d
vadniks <vadniks@fk.fk>
2022-06-07 19:27:20 +0300

    Changed a few things

--- src/main/java/some/cursov_templates/service/ComponentsService.java
+++ src/main/java/some/cursov_templates/service/ComponentsService.java
@@ -119,29 +119,6 @@ public class ComponentsService {
             image + COMPONENT_IMAGE_POSTFIX;
     }
 
-    @TestOnly
-    @Deprecated
-    public void test() {
-        repo.save(new PcComponent(
-            "Asus GeForce GTX 1650",
-            Type.GPU,
-            "Asus GeForce GTX 1650",
-            750,
-            "asus_1650_gpu"));
-        repo.save(new PcComponent(
-            "AMD Ryzen 5 5600X",
-            Type.CPU,
-            "AMD Ryzen 5 5600X",
-            700,
-            "amd_r5"));
-        repo.save(new PcComponent(
-            "AMD Ryzen 5 3600X",
-            Type.CPU,
-            "AMD Ryzen 5 3600X",
-            700,
-            "amd_r5"));
-    }
-
     public StringPairMap getComponent(Integer id) {
         val _component = repo.findById(id);
         if (_component.isEmpty()) return null;
@@ -210,5 +187,139 @@ public class ComponentsService {
     @PreDestroy
     public void preDestroy() {
         session.close();
+    }
+
+    @TestOnly
+    @Deprecated
+    public void test() {
+        repo.save(new PcComponent(
+            "Intel Core I5",
+            Type.CPU,
+            "Intel Core I5",
+            800,
+            "intel_i5"));
+        repo.save(new PcComponent(
+            "AMD Ryzen 5 5600X",
+            Type.CPU,
+            "AMD Ryzen 5 5600X",
+            700,
+            "amd_r5"));
+        repo.save(new PcComponent(
+            "AMD Ryzen 5 3600X",
+            Type.CPU,
+            "AMD Ryzen 5 3600X",
+            700,
+            "amd_r5"));
+        repo.save(new PcComponent(
+            "Asus Prime Z590-P",
+            Type.MB,
+            "Asus Prime Z590-P",
+            400,
+            "asus_prime_mb"
+        ));
+        repo.save(new PcComponent(
+            "Gigabyte Aorus X570 Elite",
+            Type.MB,
+            "Gigabyte Aorus X570 Elite",
+            450,
+            "gigabyte_aorus_mb"
+        ));
+        repo.save(new PcComponent(
+            "Maxsun Radeon RX550",
+            Type.GPU,
+            "Maxsun Radeon RX550",
+            750,
+            "maxsun_rx550_gpu"
+        ));
+        repo.save(new PcComponent(
+            "Asus GeForce GTX 1650",
+            Type.GPU,
+            "Asus GeForce GTX 1650",
+            750,
+            "asus_1650_gpu"));
+        repo.save(new PcComponent(
+            "Micron Crucial DDR4 SODIMM 8Gb",
+            Type.RAM,
+            "Micron Crucial DDR4 SODIMM 8Gb",
+            300,
+            "crucial_ram"));
+        repo.save(new PcComponent(
+            "GSkill DDR4 DIMM 16Gb",
+            Type.RAM,
+            "GSkill DDR4 DIMM 16Gb",
+            400,
+            "gskill_ram"));
+        repo.save(new PcComponent(
+            "Hitachi 1Tb 250 Mb/sec",
+            Type.HDD,
+            "Hitachi 1Tb 250 Mb/sec",
+            200,
+            "hitachi_hdd"));
+        repo.save(new PcComponent(
+            "MaxDigital 1Tb 250 Mb/sec",
+            Type.HDD,
+            "MaxDigital 1Tb 250 Mb/sec",
+            200,
+            "maxdigital_hdd"));
+        repo.save(new PcComponent(
+            "Kingston SATA 1Tb 1000Mb/Sec",
+            Type.SSD,
+            "Kingston SATA 1Tb 1000Mb/Sec",
+            500,
+            "kingston_ssd"));
+        repo.save(new PcComponent(
+            "Sabrent M2 PCIExpress 1Tb 1000Mb/Sec",
+            Type.SSD,
+            "Sabrent M2 PCIExpress 1Tb 1000Mb/Sec",
+            500,
+            "sabrent_ssd"));
+        repo.save(new PcComponent(
+            "Corsair RM 850x 500W",
+            Type.PSU,
+            "Corsair RM 850x 500W",
+            200,
+            "corsair_psu"));
+        repo.save(new PcComponent(
+            "Cooler Master MWE 750W",
+            Type.PSU,
+            "Cooler Master MWE 750W",
+            500,
+            "cooler_master_psu"));
+        repo.save(new PcComponent(
+            "Corsair A50 AM4",
+            Type.FAN,
+            "Corsair A50 AM4",
+            100,
+            "corsair_fan"));
+        repo.save(new PcComponent(
+            "Noctua R200 AM3",
+            Type.FAN,
+            "Noctua R200 AM3",
+            80,
+            "noctua_fan"));
+        repo.save(new PcComponent(
+            "Cooler Master LQD",
+            Type.WATER,
+            "Cooler Master LQD",
+            100,
+            "cooler_master_lqd_fan"));
+        repo.save(new PcComponent(
+            "Musetex ALT 905",
+            Type.CASE,
+            "Musetex ALT 905",
+            125,
+            "musetex_case"));
+        repo.save(new PcComponent(
+            "Phanteks S5",
+            Type.CASE,
+            "Phanteks S5",
+            200,
+            "phanteks_case"));
+        repo.save(new PcComponent(
+            "Thermaltake ZTX",
+            Type.CASE,
+            "Thermaltake ZTX",
+            190,
+            "thermaltake_case"));
     }
 }
